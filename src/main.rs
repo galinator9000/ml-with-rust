e43503d2b1b5b3f83b02714a2371225d7adba88a
Gali <fmehmetun3@gmail.com>
2023-11-19 20:24:49 +0300

    init candle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+
+use candle_core::{Device, Tensor};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Ma look at me I'm in Rust, multiplying tensors!");
+
+    let device = Device::Cpu;
+
+    let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
+    println!("{a}");
+
+    let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
+    println!("{b}");
+
+    let c = a.matmul(&b)?;
+    println!("{c}");
+
+    Ok(())
 }
